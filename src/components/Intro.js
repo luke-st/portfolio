f99e8e64cfda3f09f6b329a7632ededc78dbce02
Luke Stephenson <lukestephenson@live.co.uk>
2020-10-06 14:39:38 +0100

    Testing out looks

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Intro = () => {
     return (
-        <div>
-            Intro
+        <div className='box-layout'>
+            <div className='box-layout__box'>
+                    <span className='intro__content bubble'>I'm <span className='intro__name'>Luke Stephenson</span></span>
+                    <div>
+                        <span className='bubble'>Javascript Developer</span>
+                        {/* Messenger bubble format for the head? */}
+                    </div>
+                <span className='backdrop'>Hello</span>
+                <div className='intro-container'>
+                </div>
+            </div>
         </div>
     )
 }
